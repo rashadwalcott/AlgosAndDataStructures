f92f319f29594559d87d0bf6fa913378e2e14e09
Rashad <rashad.walcott15@gmail.com>
2020-01-02 13:47:24 -0500

    Made changes to third solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,12 +8,9 @@
 
 //Third Solution
 function reverse(str) {
-  let reversed = '';
-
-  for (let character of str){
-    reversed = character + reversed
-  }
-  return reversed;
+return str.split('').reduce((rev, char) => {
+  return char + rev;
+}, '');
 }
 
 
