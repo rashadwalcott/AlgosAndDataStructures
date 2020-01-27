d72155883f3e21dca19698d59189b0a514cfac89
Rashad <rashad.walcott15@gmail.com>
2020-01-26 20:18:36 -0500

    Chunks completed

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,35 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-//Second Solution
+//First Solution
+
 function chunk(array, size) {
   const chunked = [];
-  
-  let index = 0;
 
-  while (index < array.length) {
-  chunked.push(array.slice(index, index + size));
+  for(let element of array){
+    const last = chunked[chunked.length-1]
 
-  index += size;
+    if(!last || last.length === size){
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
   }
   return chunked;
 }
 
 module.exports = chunk;
 
-//First Solution
 
+
+// //Second Solution
 // function chunk(array, size) {
 //   const chunked = [];
-//
-//   for(let element of array){
-//     const last = chunked[chunked.length-1]
-//
-//     if(!last || last.length === size){
-//       chunked.push([element]);
-//     } else {
-//       last.push(element);
-//     }
+  
+//   let index = 0;
+
+//   while (index < array.length) {
+//   chunked.push(array.slice(index, index + size));
+
+//   index += size;
 //   }
 //   return chunked;
 // }
