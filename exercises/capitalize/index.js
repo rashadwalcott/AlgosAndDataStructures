796518dce48efe8d359e859f58af355a2bc1162e
Rashad <rashad.walcott15@gmail.com>
2020-10-15 11:59:01 -0400

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,29 +7,31 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-//First Solution
-function capitalize(str) {
-  const words = [];
 
-  for (let word of str.split(" ")) {
-    words.push(word[0].toUpperCase() + word.slice(1));
-  }
-  return words.join(" ");
-}
-
-//
-module.exports = capitalize;
 
 //Fav Solution
-// function capitalize(str) {
-//   let result = str[0].toUpperCase();
+function capitalize(str) {
+  let result = str[0].toUpperCase();
 
-//   for (let i = 1; i < str.length; i++) {
-//     if (str[i - 1] === " ") {
-//       result += str[i].toUpperCase();
-//     } else {
-//       result += str[i];
-//     }
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+
+module.exports = capitalize;
+
+
+//First Solution
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(" ")) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
 //   }
-//   return result;
+//   return words.join(" ");
 // }
