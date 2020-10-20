51250705f32db871a38bc19710171b0fd29366ab
Rashad <rashad.walcott15@gmail.com>
2020-10-20 11:29:04 -0400

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,9 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// Third Solution
+// 
+
+// Second Solution
 function reverse(str) {
-  return str.split("").reduce((rev, char) => char + rev, "");
+  let reversed = "";
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
 }
 
 module.exports = reverse;
@@ -19,16 +26,13 @@ module.exports = reverse;
 //   arr.reverse();
 //   return arr.join("");
 
+// Third Solution
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
+
 //   // Cleanup Code for first solution
 //   return str.split("").reverse().join("");
 // }
 
-// // Second Solution
-// function reverse(str) {
-//   let reversed = "";
 
-//   for (let character of str) {
-//     reversed = character + reversed;
-//   }
-//   return reversed;
-// }
