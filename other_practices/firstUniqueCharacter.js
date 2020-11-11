0bcf528f71290816400e676f690b19f1cc0d0e92
Rashad <rashad.walcott15@gmail.com>
2020-11-11 16:44:08 -0500

    first unique char

---
+++ other_practices/firstUniqueCharacter.js
@@ -0,0 +1,32 @@
+/*
+Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+
+*/
+
+
+//One solution
+var firstUniqChar = function(s) {
+    const hashTable = {}
+    
+    for( let char of s){
+        if( hashTable[char]) hashTable[char]++
+        else hashTable[char] = 1
+    }
+    
+    for( let i = 0; i < s.length; i++){
+        const char = s[i]
+        if(hashTable[char] === 1) return i
+    }
+    
+    return -1
+};
+
+//Second Solution
