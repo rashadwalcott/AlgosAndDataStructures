9d5436ae1ed2e8fcc4cb7ab10af7b9c039cf2e92
Rashad <rashad.walcott15@gmail.com>
2020-11-17 14:37:58 -0500

    second solution

--- other_practices/binaryTreeMaxDepth.js
+++ other_practices/binaryTreeMaxDepth.js
@@ -10,8 +10,31 @@ Example:
 Given binary tree [3,9,20,null,null,15,7],
 */
 
+function TreeNode(val){
+    this.val = val;
+    this.left = this.right = null
+}
 //One Solution
 var maxDepth = function(root) {
     if(!root) return 0
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+};
+
+//Second Solution
+var maxDepth = function(root) {
+    if(!root) return 0
+   const queue = [root]
+   let depth = 0
+   while(queue.length){
+       let size = queue.length
+       depth++
+       while(size--){
+           let item = queue.shift()
+           if(item.left) queue.push(item.left)
+           if(item.right) queue.push(item.right)
+           
+       }
+   }
+    return depth
+   
 };
