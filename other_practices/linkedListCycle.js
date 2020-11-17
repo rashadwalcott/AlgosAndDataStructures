3fceafa513a5c2b47636b7069c63f00e1e1c4ef3
Rashad <rashad.walcott15@gmail.com>
2020-11-16 19:06:45 -0500

    second solution

--- other_practices/linkedListCycle.js
+++ other_practices/linkedListCycle.js
@@ -23,3 +23,15 @@ var hasCycle = function(head) {
     }
     return false
 };
+
+//Second Solution
+var hasCycle = function(head) {
+    let slow = head
+    let fast = head
+    while(fast !== null && fast.next !== null){
+          slow = slow.next
+          fast = fast.next.next
+          if(slow === fast) return true
+    }
+        return false
+ };
