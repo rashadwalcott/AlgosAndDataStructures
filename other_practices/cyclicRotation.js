d8be29ee5cb11d6c58d4d6d383a4acdbd46d5a09
Rashad <rashad.walcott15@gmail.com>
2020-11-18 16:47:32 -0500

    cyclic rotation

---
+++ other_practices/cyclicRotation.js
@@ -0,0 +1,40 @@
+/*
+An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+
+The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+
+Write a function:
+
+class Solution { public int[] solution(int[] A, int K); }
+
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+For example, given
+
+    A = [3, 8, 9, 7, 6]
+    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+    */
+
+    //Solution
+    let arr = []
+let num = 3
+function solution(A, K){
+    if(A.length === 0) return A
+
+let i = 0;
+while( i < K){
+   let last =  A.pop()
+   A.unshift(last)
+    console.log(A)
+    i++
+
+}
+
+}
+
+solution(arr,num)
